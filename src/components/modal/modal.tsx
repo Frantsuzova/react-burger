c620162f45e8f422e4cb2e9110325ec0e8cc97a3
chizhik <afrancuzova@mail.ru>
2022-04-14 18:06:37 +0300

    fix: modal

--- src/components/modal/modal.tsx
+++ src/components/modal/modal.tsx
@@ -13,42 +13,39 @@ const Modal: FunctionComponent<{ children: React.ReactNode }> = ({ children }) =
     const location = useLocation<{ background: { pathname: string } }>()
     const { url } = useRouteMatch();
     const history = useHistory();
-    const closeIngredient = () => {
+    const closePopup = () => {
         dispatch(closeModal());
         history.replace({ pathname: location.state ? `${location.state.background.pathname}` : `${url}` });
     };
 
     const dispatch = useDispatch();
     const { allClose } = useSelector((state) => state.modalInfo)
-    const escapeClosed = useCallback(
-        (e: KeyboardEvent) => {
-            if (e.key === "Escape") {
-                dispatch(closeModal())
-            }
-        },
-        []
-    );
+    const escapeClosed = useCallback((e) => {
+        if (e.key === "Escape") {
+            closePopup();
+        }
+    }, []);
 
     useEffect(() => {
-        document.addEventListener("keydown", escapeClosed);
+        window.addEventListener("keydown", escapeClosed);
         return () => {
-            document.removeEventListener("keydown", escapeClosed);
+            window.removeEventListener("keydown", escapeClosed);
         };
     }, []);
 
     if (allClose) {
-        return null
+        return null;
     }
 
 
     return createPortal(
 
         <>
-            <ModalOverlay closeClick={() => closeIngredient()} />
+            <ModalOverlay closeClick={() => closePopup()} />
 
             <div className={styles.modal__space}>
 
-                <div className={styles.modal__close_icon} onClick={() => closeIngredient()}>
+                <div className={styles.modal__close_icon} onClick={() => closePopup()}>
                 </div>
                 <div className={styles.modalBody}>
                     {children}
